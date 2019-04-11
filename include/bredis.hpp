289997dfc05d6934c713751be0c21003b71537fd
omegacoleman <omegacoleman@gmail.com>
2019-04-11 18:40:47 +0800

    add shortcut header include/bredis.hpp

---
+++ include/bredis.hpp
@@ -0,0 +1,13 @@
+#ifndef BREDIS_HPP
+#define BREDIS_HPP
+
+#include <bredis/Command.hpp>
+#include <bredis/Connection.hpp>
+#include <bredis/Error.hpp>
+#include <bredis/Extract.hpp>
+#include <bredis/MarkerHelpers.hpp>
+#include <bredis/Markers.hpp>
+#include <bredis/Protocol.hpp>
+#include <bredis/Result.hpp>
+
+#endif // BREDIS_HPP
